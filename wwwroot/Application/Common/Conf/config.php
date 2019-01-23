72d2f8e187640653da84014fdb5399ff60cd235b
jun <100@qq.com>
2019-01-23 11:02:18 +0800

    ``

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -63,8 +63,8 @@ return array(
      /* 发邮件配置 */
         'MAIL_ADDRESS'=>'q128402131@126.com', // 邮箱地址
         'MAIL_SMTP'=>'smtp.126.com', // 邮箱SMTP服务器
-        'MAIL_LOGINNAME'=>'q128402131', // 邮箱登录帐号
-        'MAIL_PASSWORD'=>'jun820605', // 邮箱密码
+        'MAIL_LOGINNAME'=>'', // 邮箱登录帐号
+        'MAIL_PASSWORD'=>'', // 邮箱密码
         'MAIL_CHARSET'=>'UTF-8',//编码
         'MAIL_AUTH'=>true,//邮箱认证
         'MAIL_HTML'=>true,//true HTML格式 false TXT格式
